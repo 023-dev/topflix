4219c05ab7a2c37ef8fa45f92463be55a106d32a
qordpsem <qordpsem@gmail.com>
2024-07-29 20:16:33 +0900

    [Feat] 회원탈퇴 기능 구현
    1. 리디렉션 초과돼서 탈퇴되는건 확인했는데,
    마이페이지에서 회원탈퇴로 처음 넘어갈때 alert 뜨는 문제가 잡혔는지 다른 팀원분의 확인 부탁드립니다,,,
    
    WithdrawUserAction 의 19번째 줄도,, 한번 확인을,,

---
+++ src/main/java/com/topflix/action/WithdrawUserAction.java
@@ -0,0 +1,43 @@
+package com.topflix.action;
+
+import com.topflix.repository.UserRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class WithdrawUserAction implements Action {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserRepository userRepository = new UserRepository();
+        HttpSession session = request.getSession();
+        String email = (String) session.getAttribute("userEmail");
+        String password = request.getParameter("password");
+
+        if (email == null) { //  email.isEmpty 로 변경 예정
+            request.setAttribute("message", "로그인 세션이 만료되었습니다.");
+            return "signIn.do";
+        }
+
+        if (password == null){
+            return "withdrawUser.do";
+        }
+
+        int isMember = userRepository.isMember(email, password);
+        System.out.println(isMember);
+        if (isMember == 1) { // 비번일치 시
+            int result = userRepository.withdraw(email);
+            if (result == 1) {// 탈퇴 성공
+                request.setAttribute("message", "탈퇴 되었습니다. 안녕...");
+            } else { // 탈퇴 실패
+                request.setAttribute("message", "탈퇴에 실패하였습니다.");
+            }
+        } else {
+            request.setAttribute("message", "비밀번호가 올바르지 않습니다.");
+        }
+        return "withdrawUser.jsp";
+    }
+}
+
