eff6689e570069c6ec1bc35ff945bd9fb946317f
sxu <bnsxu@naver.com>
2024-07-30 10:21:28 +0900

    myPage, footer 수정

--- src/main/java/com/topflix/action/MyPageAction.java
+++ src/main/java/com/topflix/action/MyPageAction.java
@@ -1,14 +1,32 @@
 package com.topflix.action;
 
+import com.topflix.domain.User;
+import com.topflix.repository.UserRepository;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class MyPageAction implements Action{
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession httpSession = request.getSession();
+        UserRepository userRepository = new UserRepository();
+        String userEmail = (String) httpSession.getAttribute("userEmail");
+
+        User user = userRepository.getUserByEmail(userEmail);
+        String name = user.getName();
+        String email = user.getEmail();
+        String phone = user.getPhone();
+
+        request.setAttribute("user", user);
+        request.setAttribute("name", name);
+        request.setAttribute("email", email);
+        request.setAttribute("phone", phone);
+
         return "/views/myPage.jsp";
     }
 }
--- src/main/webapp/includes/footer.jsp
+++ src/main/webapp/includes/footer.jsp
@@ -13,12 +13,12 @@
             <a href="#">고객 센터</a>
             <a href="#">팀 정보</a>
         </div>
-        <img src="../images/lang.png" alt="한국어" class="language-selector">
-        <div class="footer-section">탑플릭스 대한민국</div>
-        <div class="footer-section">탑플릭스서비스코리아 유한회사 통신판매업신고번호: 제0000-서울종로-0000호</div>
+        <div class="footer-section topflix">탑플릭스 대한민국</div>
+        <div class="footer-section">탑플릭스서비스코리아 KOSTA</div>
+        <div class="footer-section">통신판매업신고번호: 제0000-서울종로-0000호</div>
         <div class="footer-section">팀장: 이동준 팀원: 공희상 박규희 변시우</div>
         <div class="footer-section">주소: 서울시 종로구 우정국로21 대왕빌딩 7층 702호</div>
-        <div class="footer-section cloud-hosting">클라우드 호스팅: <span>Amazon Web Services Inc.</span></div>
+        <div class="footer-section cloud-hosting">클라우드 호스팅: <span>Kosta Web Services Inc.</span></div>
     </div>
 </div>
 </body>
